ad2ee0795dcdd4a0156f35d994febeb173f523a5
ThaUnknown <6506529+ThaUnknown@users.noreply.github.com>
2025-09-02 16:48:32 +0200

    fix: revert torrentport [did nothing]

--- src/preload/index.ts
+++ src/preload/index.ts
@@ -1,22 +1,22 @@
+import { proxy } from 'abslink'
 import { wrap } from 'abslink/electron'
+import { wrap as wrapPort } from 'abslink/w3c'
 import { contextBridge, ipcRenderer } from 'electron'
 
 import type IPC from '../main/ipc.ts'
+import type { Remote } from 'abslink'
 import type { Native } from 'native'
+import type TorrentClient from 'torrent-client'
 
 ipcRenderer.send('preload-done')
 
-const windowLoaded = new Promise<void>(resolve => {
-  contextBridge.exposeInMainWorld('sendPort', () => resolve())
+const torrent = new Promise<Remote<TorrentClient>>(resolve => {
+  ipcRenderer.once('port', ({ ports }) => {
+    if (!ports[0]) return
+    ports[0].start()
+    resolve(wrapPort<TorrentClient>(ports[0]) as unknown as Remote<TorrentClient>)
+  })
 })
-
-ipcRenderer.once('port', async ({ ports }) => {
-  if (!ports[0]) return
-  await windowLoaded
-  // eslint-disable-next-line no-undef
-  window.postMessage('TORRENT_PORT', '*', ports)
-})
-
 const version = ipcRenderer.invoke('version')
 
 const main = wrap<typeof IPC.prototype>(ipcRenderer)
@@ -26,8 +26,7 @@ const native: Partial<Native> = {
   selectPlayer: () => main.selectPlayer(),
   selectDownload: async () => {
     const path = await main.selectDownload()
-    // TODO
-    // await (await torrent).verifyDirectoryPermissions(path)
+    await (await torrent).verifyDirectoryPermissions(path)
     return path
   },
   setAngle: (angle: string) => main.setAngle(angle),
@@ -53,7 +52,22 @@ const native: Partial<Native> = {
     navigator.mediaSession.playbackState = e
     await main.setPlayBackState(e)
   },
+  checkAvailableSpace: async () => await (await torrent).checkAvailableSpace(),
+  checkIncomingConnections: async (port) => await (await torrent).checkIncomingConnections(port),
+  updatePeerCounts: async (hashes) => await (await torrent).scrape(hashes),
+  playTorrent: async (id, mediaID, episode) => await (await torrent).playTorrent(id, mediaID, episode),
+  library: async () => await (await torrent).library(),
+  attachments: async (hash, id) => await (await torrent).attachments.attachments(hash, id),
+  tracks: async (hash, id) => await (await torrent).attachments.tracks(hash, id),
+  subtitles: async (hash, id, cb) => await (await torrent).attachments.subtitle(hash, id, proxy(cb)),
+  errors: async (cb) => await (await torrent).errors(proxy(cb)),
+  chapters: async (hash, id) => await (await torrent).attachments.chapters(hash, id),
+  torrentInfo: async (hash) => await (await torrent).torrentInfo(hash),
+  peerInfo: async (hash) => await (await torrent).peerInfo(hash),
+  fileInfo: async (hash) => await (await torrent).fileInfo(hash),
+  protocolStatus: async (hash) => await (await torrent).protocolStatus(hash),
   updateSettings: (settings) => main.updateSettings(settings),
+  cachedTorrents: async () => await (await torrent).cached(),
   setHideToTray: (enabled: boolean) => main.setHideToTray(enabled),
   isApp: true,
   spawnPlayer: (url) => main.spawnPlayer(url),
@@ -76,7 +90,8 @@ const native: Partial<Native> = {
     if (!data) return
     await navigator.clipboard.writeText(data.url ?? data.text ?? data.title!)
   },
-  defaultTransparency: () => false
+  defaultTransparency: () => false,
+  debug: async (levels) => await (await torrent).debug(levels)
 }
 
 try {
