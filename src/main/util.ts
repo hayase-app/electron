46c9f5c67826da54e01e0b93aed2b4edfecfba99
ThaUnknown <6506529+ThaUnknown@users.noreply.github.com>
2025-12-28 16:56:30 +0100

    feat: fuses feat: wasm resizable buffers fix: enable webGPU on linux

--- src/main/util.ts
+++ src/main/util.ts
@@ -1,6 +1,11 @@
+import process from 'node:process'
+
 import { app } from 'electron'
 
 import store from './store.ts'
+
+// safe performance stuff
+const enableFeatures: [string, string] = ['enable-features', 'PlatformEncryptedDolbyVision,CanvasOopRasterization,ThrottleDisplayNoneAndVisibilityHiddenCrossOriginIframes,UseSkiaRenderer,WebAssemblyLazyCompilation,FluentOverlayScrollbar,WindowsScrollingPersonality,AutoPictureInPictureForVideoPlayback']
 
 const flags: Array<[string, string | undefined]| [string]> = [
   // not sure if safe?
@@ -9,10 +14,9 @@ const flags: Array<[string, string | undefined]| [string]> = [
   // ['force-gpu-mem-available-mb=2048'],
   // should be safe
   ['enable-hardware-overlays', 'single-fullscreen,single-on-top,underlay'],
-  // safe performance stuff
-  ['enable-features', 'PlatformEncryptedDolbyVision,CanvasOopRasterization,ThrottleDisplayNoneAndVisibilityHiddenCrossOriginIframes,UseSkiaRenderer,WebAssemblyLazyCompilation,FluentOverlayScrollbar,WindowsScrollingPersonality,AutoPictureInPictureForVideoPlayback'],
-  // disabling shit, vulkan rendering, widget layering aka right click context menus [I think] for macOS [I think]
-  ['disable-features', 'Vulkan,WidgetLayering'], // ,MediaEngagementBypassAutoplayPolicies,PreloadMediaEngagementData,RecordMediaEngagementScores might not be good
+  enableFeatures,
+  // disabling shit widget layering aka right click context menus [I think] for macOS [I think]
+  ['disable-features', 'WidgetLayering'], // ,MediaEngagementBypassAutoplayPolicies,PreloadMediaEngagementData,RecordMediaEngagementScores might not be good,
   // utility stuff, aka website security that's useless for a native app:
   ['autoplay-policy', 'no-user-gesture-required'], ['disable-notifications'], ['disable-logging'], ['disable-permissions-api'], ['no-zygote'],
   // bypasses W3C API permissions which require visiblity to run, IE local fonts, this DOES NOT disable background throttling and thus doesnt break pause on lost visibility
@@ -23,8 +27,18 @@ const flags: Array<[string, string | undefined]| [string]> = [
   ['disk-cache-size', '500000000'],
   // NodeJS security stuff, not supported yet
   ['disallow-code-generation-from-strings'], ['disable-proto', 'throw'], ['frozen-intrinsics'],
-  ['js-flags', '--disallow-code-generation-from-strings']
+  ['js-flags', '--disallow-code-generation-from-strings --experimental-wasm-rab-integration']
 ]
+
+const linuxPlatforms = ['linux', 'freebsd', 'openbsd', 'netbsd', 'dragonfly', 'sunos']
+
+if (linuxPlatforms.includes(process.platform)) {
+  flags.push(['enable-unsafe-webgpu'])
+  enableFeatures[1] += ',Vulkan,VulkanFromANGLE'
+} else if (process.platform === 'win32' && process.arch === 'arm64') {
+  flags.push(['enable-unsafe-webgpu'])
+}
+
 for (const [flag, value] of flags) {
   app.commandLine.appendSwitch(flag, value)
 }
